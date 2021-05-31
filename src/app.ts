f267e71830bdf3c22d9acd99b58396fa9bb0d6b4
Jimmy Zhang <jimmy.zhang@microsoft.com>
2021-05-30 23:38:44 -0700

    cleanup startup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 import * as UI from "./ui";
 
+export type TrackDescriptor = {
+  name: string,
+  uri: string,
+  artist?: string
+};
+
 const fetch = require('node-fetch');
+const DEFAULT_TRACK: TrackDescriptor = {name: 'Street Hoops World Theme', uri: 'self_and_other_loop.ogg', artist: 'Altspace'};
 const DEBUG = true;
 
 export default class App {
@@ -10,9 +17,10 @@ export default class App {
   private trackPlaying = false;
   private trackSoundInstance: MRE.MediaInstance = null;
   private trackIndex = 0;
-  public totalTracks = 0;
   private currentTrack: MRE.Sound = null;
+  private defaultMultiple = false;
 
+  public totalTracks = 0;
   public buttonPlay: MRE.Actor;
   public buttonPrevious: MRE.Actor;
   public buttonNext: MRE.Actor;
@@ -26,9 +34,29 @@ export default class App {
 	}
 
 	private async started() {
+    this.params.test = 'content_pack';
+
+    switch(this.params.test){
+      case 'content_pack':
+        this.params.content_pack = '1748961296881025140';
+        break;
+      default:
+        break;
+    }
+
     await this.loadTracks();
+
+    if(DEBUG){ console.log(this.tracks) }
+
+    // cache this for next/previous buttons
+    this.totalTracks = this.assets.sounds.length;
+
+    // choose the first track
+    this.currentTrack = this.tracks[0];
+
+    // controls
     UI.createBoombox(this);
-    await this.wireUpButtons();
+    this.wireUpButtons();
 	}
 
   private userLeft(user: MRE.User) {
@@ -37,20 +65,72 @@ export default class App {
   private userJoined(user: MRE.User) {
   }
 
-  private async loadTracks() {
-    this.tracks.push(await this.assets.createSound('Track1', { uri: 'self_and_other_loop.ogg' }));
-    this.tracks.push(await this.assets.createSound('Track2', { uri: 'JazzLoopstereo.ogg' }));
-    this.tracks.push(await this.assets.createSound('Track3', { uri: 'https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1168441484869894861/inner_light.ogg' }));
+  private async loadTracks(){
+    // load both import options in parallel
+    // => should only be using one option
+    await Promise.all([
+      this.loadContentPack(),
+      this.loadStation()
+    ]).catch();
 
-    // calculate total for next/previous buttons
-    this.totalTracks = this.assets.sounds.length;
-
-    // setup default track
-    this.currentTrack = this.tracks[this.trackIndex];
+    await this.loadDefault();
   }
 
+  private async loadContentPack(){
+    if(!this.params.content_pack){ return }
+
+    let uri = 'https://account.altvr.com/api/content_packs/' + this.params.content_pack + '/raw.json';
+
+    await fetch(uri)
+      .then((res: any) => res.json())
+      .then((json: any) => {
+        let importedTracks = Object.assign({}, json).tracks;
+        if(!importedTracks){ return }
+        for(let i=0; i < importedTracks.length; i++){
+          let track = importedTracks[i];
+          this.tracks.push(this.assets.createSound(track.name, { uri: track.uri }));
+        }
+        if(DEBUG){ console.log(`Imported ${this.tracks.length} track(s) from ${uri}`) }
+      })
+  }
+
+  private async loadDefault() {
+    if(this.assets.sounds.length > 0){ return }
+
+    if(DEBUG){ console.log('Loading default tracks') }
+
+    this.tracks.push(await this.assets.createSound(DEFAULT_TRACK.name, { uri: DEFAULT_TRACK.uri }));
+    // this.tracks.push(await this.assets.createSound('Track1', { uri: 'self_and_other_loop.ogg' }));
+    // this.tracks.push(await this.assets.createSound('Track2', { uri: 'JazzLoopstereo.ogg' }));
+    // this.tracks.push(await this.assets.createSound('Track3', { uri: 'https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1168441484869894861/inner_light.ogg' }));
+  }
+
+  private async loadStation(){
+    // if(!params.poll){ return }
+
+    // let json = null;
+
+    // // handle if the poll doesn't exist
+    // try{
+    //   json = require(`../polls/${params.poll}.json`);
+    // }
+    // catch{
+    //   return;
+    // }
+
+    // if(DEBUG){ console.log(json) };
+
+    // let importedPolls = Object.assign({}, json).favorites;
+
+    // if(DEBUG){ console.log(importedPolls) };
+
+    // if(!importedPolls){ return }
+    // this.createFavoritesButtonFor(this.context, user, importedPolls);
+  }
+
+
   private async playTrack(track: MRE.Sound) {
-    if(DEBUG){ console.log(`Track: ${track.name} - ${this.trackIndex + 1} of ${this.totalTracks}`) }
+    if(DEBUG){ console.log(`[JimmyRadio][Playing] ${track.name} - ${this.trackIndex + 1} of ${this.totalTracks}`) }
     this.trackSoundInstance = this.buttonPlay.startSound(track.id,
       { volume: .25, looping: true, doppler: 0, spread: 0.75, });
   }
@@ -122,43 +202,6 @@ export default class App {
     this.trackPlaying = true;
   }
 
-  private loadContentPack(params: MRE.ParameterSet, user: MRE.User){
-    // if(!params.content_pack){ return }
-    // let uri = 'https://account.altvr.com/api/content_packs/' + params.content_pack + '/raw.json';
-
-    // fetch(uri)
-    //   .then((res: any) => res.json())
-    //   .then((json: any) => {
-    //     let importedPolls = Object.assign({}, json).favorites;
-    //     if(!importedPolls){ return }
-    //     this.createFavoritesButtonFor(this.context, user, importedPolls);
-    //   })
-  }
-
-  // Load Favorites from the /polls folder if you pass ?poll=<name>
-  // e.g. ws://10.0.1.119:3901?poll=quickstart
-  private loadBundledPoll(params: MRE.ParameterSet, user: MRE.User){
-    // if(!params.poll){ return }
-
-    // let json = null;
-
-    // // handle if the poll doesn't exist
-    // try{
-    //   json = require(`../polls/${params.poll}.json`);
-    // }
-    // catch{
-    //   return;
-    // }
-
-    // if(DEBUG){ console.log(json) };
-
-    // let importedPolls = Object.assign({}, json).favorites;
-
-    // if(DEBUG){ console.log(importedPolls) };
-
-    // if(!importedPolls){ return }
-    // this.createFavoritesButtonFor(this.context, user, importedPolls);
-  }
 
   private canManageRadio(user: MRE.User) : boolean{
     let roles = user.properties['altspacevr-roles'].split(',');
